0adbc889b29607ce46292084f4785b94abcdc11b
Elias Projahn <johrpan@gmail.com>
2020-10-09 20:19:46 +0200

    Simplify state management for main window

--- src/window.rs
+++ src/window.rs
@@ -7,41 +7,50 @@ use gtk::prelude::*;
 use gtk_macros::{action, get_widget};
 use libhandy::prelude::*;
 use libhandy::HeaderBarExt;
-use std::cell::RefCell;
+use std::cell::Cell;
 use std::convert::TryInto;
 use std::rc::Rc;
+
+enum WindowState {
+    Loading,
+    Persons(Vec<Person>),
+    PersonLoading(Person),
+    Person(Vec<WorkDescription>, Vec<RecordingDescription>),
+}
 
 pub struct Window {
     window: libhandy::ApplicationWindow,
     backend: Rc<Backend>,
     leaflet: libhandy::Leaflet,
-    persons: RefCell<Vec<Person>>,
-    works: RefCell<Vec<WorkDescription>>,
-    recordings: RefCell<Vec<RecordingDescription>>,
-    sidebar_box: gtk::Box,
+    sidebar_stack: gtk::Stack,
     person_search_entry: gtk::SearchEntry,
     person_list: gtk::ListBox,
     stack: gtk::Stack,
     header: libhandy::HeaderBar,
     header_menu_button: gtk::MenuButton,
+    content_stack: gtk::Stack,
     work_box: gtk::Box,
     work_list: gtk::ListBox,
     recording_box: gtk::Box,
     recording_list: gtk::ListBox,
+    person_list_row_activated_handler_id: Cell<Option<glib::SignalHandlerId>>,
 }
 
 impl Window {
     pub fn new(app: &gtk::Application) -> Rc<Self> {
+        use WindowState::*;
+
         let builder = gtk::Builder::from_resource("/de/johrpan/musicus_editor/ui/window.ui");
 
         get_widget!(builder, libhandy::ApplicationWindow, window);
         get_widget!(builder, libhandy::Leaflet, leaflet);
-        get_widget!(builder, gtk::Box, sidebar_box);
         get_widget!(builder, gtk::SearchEntry, person_search_entry);
+        get_widget!(builder, gtk::Stack, sidebar_stack);
         get_widget!(builder, gtk::ListBox, person_list);
         get_widget!(builder, gtk::Stack, stack);
         get_widget!(builder, libhandy::HeaderBar, header);
         get_widget!(builder, gtk::MenuButton, header_menu_button);
+        get_widget!(builder, gtk::Stack, content_stack);
         get_widget!(builder, gtk::Box, work_box);
         get_widget!(builder, gtk::ListBox, work_list);
         get_widget!(builder, gtk::Box, recording_box);
@@ -53,48 +62,19 @@ impl Window {
             window: window,
             backend: Rc::new(backend),
             leaflet: leaflet,
-            persons: RefCell::new(Vec::new()),
-            works: RefCell::new(Vec::new()),
-            recordings: RefCell::new(Vec::new()),
-            sidebar_box: sidebar_box,
+            sidebar_stack: sidebar_stack,
             person_list: person_list,
             person_search_entry: person_search_entry,
             stack: stack,
             header: header,
             header_menu_button: header_menu_button,
+            content_stack: content_stack,
             work_box: work_box,
             work_list: work_list,
             recording_box: recording_box,
             recording_list: recording_list,
+            person_list_row_activated_handler_id: Cell::new(None),
         });
-
-        result
-            .person_list
-            .connect_row_activated(clone!(@strong result => move |_, row| {
-                let row = row.get_child().unwrap().downcast::<SelectorRow>().unwrap();
-                let index: usize = row.get_index().try_into().unwrap();
-                let person = result.persons.borrow()[index].clone();
-                result.show_person(person);
-            }));
-
-        result
-            .person_list
-            .set_filter_func(Some(Box::new(clone!(@strong result => move |row| {
-                let row = row.get_child().unwrap().downcast::<SelectorRow>().unwrap();
-                let index: usize = row.get_index().try_into().unwrap();
-                let search = result.person_search_entry.get_text().to_string().to_lowercase();
-
-                search.is_empty() || result.persons.borrow()[index]
-                    .name_lf()
-                    .to_lowercase()
-                    .contains(&search)
-            }))));
-
-        result
-            .person_search_entry
-            .connect_search_changed(clone!(@strong result => move |_| {
-                result.person_list.invalidate_filter();
-            }));
 
         action!(
             result.window,
@@ -109,10 +89,7 @@ impl Window {
             "add-person",
             clone!(@strong result => move |_, _| {
                 PersonEditor::new(result.backend.clone(), &result.window, None, clone!(@strong result => move |_| {
-                    result.backend.get_persons(clone!(@strong result => move |persons| {
-                        result.persons.replace(persons);
-                        result.show_persons();
-                    }));
+                   result.clone().set_state(Loading);
                 })).show();
             })
         );
@@ -132,10 +109,7 @@ impl Window {
             "add-work",
             clone!(@strong result => move |_, _| {
                 WorkEditor::new(result.backend.clone(), &result.window, None, clone!(@strong result => move |_| {
-                    result.backend.get_persons(clone!(@strong result => move |persons| {
-                        result.persons.replace(persons);
-                        result.show_persons();
-                    }));
+                    result.clone().set_state(Loading);
                 })).show();
             })
         );
@@ -161,11 +135,8 @@ impl Window {
             clone!(@strong result => move |_, id| {
                 result.backend.get_person(id.unwrap().get().unwrap(), clone!(@strong result => move |person| {
                     let person = person.unwrap();
-                    PersonEditor::new(result.backend.clone(), &result.window, Some(person), clone!(@strong result => move |person| {
-                        result.backend.get_persons(clone!(@strong result => move |persons| {
-                            result.persons.replace(persons);
-                            result.show_persons();
-                        }));
+                    PersonEditor::new(result.backend.clone(), &result.window, Some(person), clone!(@strong result => move |_| {
+                        result.clone().set_state(Loading);
                     })).show();
                 }));
             })
@@ -177,17 +148,19 @@ impl Window {
             Some(glib::VariantTy::new("x").unwrap()),
             clone!(@strong result => move |_, id| {
                 result.backend.delete_person(id.unwrap().get().unwrap(), clone!(@strong result => move |_| {
-                    result.back();
-                    result.backend.get_persons(clone!(@strong result => move |persons| {
-                        result.persons.replace(persons);
-                        result.show_persons();
-                    }));
+                    result.clone().set_state(Loading);
                 }));
             })
         );
 
+        result
+            .person_search_entry
+            .connect_search_changed(clone!(@strong result => move |_| {
+                result.person_list.invalidate_filter();
+            }));
+
         result.window.set_application(Some(app));
-        result.show_persons();
+        result.clone().set_state(Loading);
 
         result
     }
@@ -196,85 +169,133 @@ impl Window {
         self.window.present();
     }
 
-    fn show_persons(&self) {
-        for child in self.person_list.get_children() {
-            self.person_list.remove(&child);
-        }
+    fn set_state(self: Rc<Self>, state: WindowState) {
+        use WindowState::*;
 
-        for (index, person) in self.persons.borrow().iter().enumerate() {
-            let label = gtk::Label::new(Some(&person.name_lf()));
-            label.set_halign(gtk::Align::Start);
-            let row = SelectorRow::new(index.try_into().unwrap(), &label);
-            row.show_all();
-            self.person_list.insert(&row, -1);
-        }
-    }
+        match state {
+            Loading => {
+                self.backend
+                    .get_persons(clone!(@strong self as self_ => move |persons| {
+                        self_.clone().set_state(Persons(persons));
+                    }));
 
-    fn show_person(&self, person: Person) {
-        self.header.set_title(Some(&person.name_fl()));
-        let edit_menu_item = gio::MenuItem::new(Some("Edit person"), None);
-        edit_menu_item.set_action_and_target_value(
-            Some("win.edit-person"),
-            Some(&glib::Variant::from(person.id)),
-        );
-        let delete_menu_item = gio::MenuItem::new(Some("Delete person"), None);
-        delete_menu_item.set_action_and_target_value(
-            Some("win.delete-person"),
-            Some(&glib::Variant::from(person.id)),
-        );
-        let menu = gio::Menu::new();
-        menu.append_item(&edit_menu_item);
-        menu.append_item(&delete_menu_item);
+                self.sidebar_stack.set_visible_child_name("loading");
+                self.stack.set_visible_child_name("empty_screen");
+                self.leaflet.set_visible_child_name("sidebar");
+            }
+            Persons(persons) => {
+                for child in self.person_list.get_children() {
+                    self.person_list.remove(&child);
+                }
 
-        self.header_menu_button.set_menu_model(Some(&menu));
+                for (index, person) in persons.iter().enumerate() {
+                    let label = gtk::Label::new(Some(&person.name_lf()));
+                    label.set_halign(gtk::Align::Start);
+                    let row = SelectorRow::new(index.try_into().unwrap(), &label);
+                    row.show_all();
+                    self.person_list.insert(&row, -1);
+                }
 
-        // let result = self.clone();
-        // self.backend.get_work_descriptions(person.id, |works| {
-        //     result.show_works();
-        //     result.show_recordings();
-        //     result.stack.set_visible_child_name("person_screen");
-        //     result.leaflet.set_visible_child(&result.stack);
-        // });
-    }
+                match self.person_list_row_activated_handler_id.take() {
+                    Some(id) => self.person_list.disconnect(id),
+                    None => (),
+                }
 
-    fn show_works(&self) {
-        for child in self.work_list.get_children() {
-            self.work_list.remove(&child);
-        }
+                let handler_id = self.person_list.connect_row_activated(
+                    clone!(@strong self as self_, @strong persons => move |_, row| {
+                        let row = row.get_child().unwrap().downcast::<SelectorRow>().unwrap();
+                        let index: usize = row.get_index().try_into().unwrap();
+                        let person = persons[index].clone();
+                        self_.clone().set_state(PersonLoading(person));
+                    }),
+                );
 
-        let works = self.works.borrow();
+                self.person_list_row_activated_handler_id
+                    .set(Some(handler_id));
 
-        if works.is_empty() {
-            self.work_box.hide();
-        } else {
-            self.work_box.show();
-        }
+                self.person_list.set_filter_func(Some(Box::new(
+                    clone!(@strong self as self_, @strong persons => move |row| {
+                        let row = row.get_child().unwrap().downcast::<SelectorRow>().unwrap();
+                        let index: usize = row.get_index().try_into().unwrap();
+                        let search = self_.person_search_entry.get_text().to_string().to_lowercase();
 
-        for (index, work) in works.iter().enumerate() {
-            let label = gtk::Label::new(Some(&work.title));
-            label.set_halign(gtk::Align::Start);
-            let row = SelectorRow::new(index.try_into().unwrap(), &label);
-            row.show_all();
-            self.work_list.insert(&row, -1);
-        }
-    }
+                        search.is_empty() || persons[index]
+                            .name_lf()
+                            .to_lowercase()
+                            .contains(&search)
+                    }),
+                )));
 
-    fn show_recordings(&self) {
-        for child in self.recording_list.get_children() {
-            self.recording_list.remove(&child);
-        }
+                self.sidebar_stack.set_visible_child_name("persons_list");
+                self.stack.set_visible_child_name("empty_screen");
+                self.leaflet.set_visible_child_name("sidebar");
+            }
+            PersonLoading(person) => {
+                self.header.set_title(Some(&person.name_fl()));
 
-        let recordings = self.recordings.borrow();
+                let edit_menu_item = gio::MenuItem::new(Some("Edit person"), None);
+                edit_menu_item.set_action_and_target_value(
+                    Some("win.edit-person"),
+                    Some(&glib::Variant::from(person.id)),
+                );
 
-        if recordings.is_empty() {
-            self.recording_box.hide();
-        } else {
-            self.recording_box.show();
+                let delete_menu_item = gio::MenuItem::new(Some("Delete person"), None);
+                delete_menu_item.set_action_and_target_value(
+                    Some("win.delete-person"),
+                    Some(&glib::Variant::from(person.id)),
+                );
+
+                let menu = gio::Menu::new();
+                menu.append_item(&edit_menu_item);
+                menu.append_item(&delete_menu_item);
+
+                self.header_menu_button.set_menu_model(Some(&menu));
+
+                self.backend.get_work_descriptions(
+                    person.id,
+                    clone!(@strong self as self_ => move |works| {
+                        self_.clone().set_state(Person(works, Vec::new()));
+                    }),
+                );
+
+                self.content_stack.set_visible_child_name("loading");
+                self.stack.set_visible_child_name("person_screen");
+                self.leaflet.set_visible_child_name("content");
+            }
+            Person(works, recordings) => {
+                for child in self.work_list.get_children() {
+                    self.work_list.remove(&child);
+                }
+
+                if works.is_empty() {
+                    self.work_box.hide();
+                } else {
+                    self.work_box.show();
+                }
+
+                for (index, work) in works.iter().enumerate() {
+                    let label = gtk::Label::new(Some(&work.title));
+                    label.set_halign(gtk::Align::Start);
+                    let row = SelectorRow::new(index.try_into().unwrap(), &label);
+                    row.show_all();
+                    self.work_list.insert(&row, -1);
+                }
+
+                if recordings.is_empty() {
+                    self.recording_box.hide();
+                } else {
+                    self.recording_box.show();
+                }
+
+                self.content_stack.set_visible_child_name("content");
+                self.stack.set_visible_child_name("person_screen");
+                self.leaflet.set_visible_child_name("content");
+            }
         }
     }
 
     fn back(&self) {
         self.stack.set_visible_child_name("empty_screen");
-        self.leaflet.set_visible_child(&self.sidebar_box);
+        self.leaflet.set_visible_child_name("sidebar");
     }
 }
