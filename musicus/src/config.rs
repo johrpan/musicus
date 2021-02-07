5c77ef95f737943b3a64662e468a4308f0bbb6ee
Elias Projahn <johrpan@gmail.com>
2021-02-07 12:55:11 +0100

    Remove config and resources from version control

--- musicus/src/config.rs
+++
@@ -1,2 +0,0 @@
-pub static VERSION: &str = "0.1.0";
-pub static LOCALEDIR: &str = "/usr/local/share/locale";
--- musicus/src/resources.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-
-pub fn init() -> Result<()> {
-    let bytes = glib::Bytes::from(include_bytes!("/home/johrpan/Entwicklung/musicus/build/musicus/res/musicus.gresource").as_ref());
-    let resource = gio::Resource::from_data(&bytes)?;
-    gio::resources_register(&resource);
-
-    Ok(())
-}
