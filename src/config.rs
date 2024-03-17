5a74b0ca5cad2c36032e8433682dfba3d7be7188
Elias Projahn <elias@johrpan.de>
2024-03-17 11:09:08 +0100

    Add config.rs to .gitignore

--- src/config.rs
+++
@@ -1,4 +0,0 @@
-pub static VERSION: &str = "0.1.0";
-pub static GETTEXT_PACKAGE: &str = "musicus";
-pub static LOCALEDIR: &str = "/app/share/locale";
-pub static PKGDATADIR: &str = "/app/share/musicus";
