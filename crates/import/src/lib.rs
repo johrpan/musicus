9969fb065e4b3d6aa4ad1982afd92fbdd5012444
Elias Projahn <elias@johrpan.de>
2025-02-09 10:03:38 +0100

    Prepare for switching to new branch

--- crates/import/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub use error::{Error, Result};
-pub use session::{ImportSession, ImportTrack, State};
-
-pub mod error;
-pub mod session;
-
-mod disc;
-mod folder;
--- crates/musicus/src/import/mod.rs
+++
@@ -1,9 +0,0 @@
-mod import_screen;
-mod medium_editor;
-mod medium_preview;
-mod source_selector;
-mod track_editor;
-mod track_selector;
-mod track_set_editor;
-
-pub use source_selector::SourceSelector;
