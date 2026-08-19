2d50571f953b36764e76d203e64d039e06384a42
Elias Projahn <elias@johrpan.de>
2026-08-19 11:47:04 +0200

    library: Reset database schema for v1.0

--- migrations/2026-08-12-090000_tag_private/down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE tags DROP COLUMN private;
-
-UPDATE meta SET schema_version = 3, updated_at = DATETIME('now') WHERE id = 1;
--- migrations/2026-08-17-090000_work_relates_to/down.sql
+++
@@ -1,5 +0,0 @@
-DROP INDEX works_relates_to;
-
-ALTER TABLE works DROP COLUMN relates_to;
-
-UPDATE meta SET schema_version = 5, updated_at = DATETIME('now') WHERE id = 1;
